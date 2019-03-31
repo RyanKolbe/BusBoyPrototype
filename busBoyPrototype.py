1511bfa32964e44cf3104d65c9288eb74cd7d122
Jonathan Theron <jonathant@webafrica.com>
2019-03-31 15:35:59 +0200

    round2

---
+++ busBoyPrototype.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+top = Tk()
+top.geometry("600x400")
+
+tabel1BTN = Button(top, text="1", bg="green")
+tabel1BTN.pack()
+
+tabel2BTN = Button(top, text="2", bg="green")
+tabel2BTN.pack()
+
+tabel3BTN = Button(top, text="3", bg="green")
+tabel3BTN.pack()
+
+top.mainloop()
